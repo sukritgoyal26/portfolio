44258b725af6a10149caf894e61f0fa7229f3b00
Sukrit <sukritgoyal36@gmail.com>
2023-12-01 15:53:28 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Router>
       <NavBar />
         <Routes>
-          <Route path='/portfolio/' element = {<Home />} />
-          <Route path='/portfolio/projects' element = {<Projects />} />
-          <Route path='/portfolio/experience' element = {<Experience />} />
+          <Route path='/' element = {<Home />} />
+          <Route path='/projects' element = {<Projects />} />
+          <Route path='/experience' element = {<Experience />} />
 
         </Routes>
         <Footer />
