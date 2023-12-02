c08498c8334eca2e4cbe94eec1bda0e1b7b36396
Sukrit <sukritgoyal36@gmail.com>
2023-12-02 22:21:02 +0530

    s

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/projects" element = {<Projects />} />
           <Route path="/experience" element = {<Experience />} />
         </Routes>
-        </Router>
+      </Router>
         <Footer />
         
     </div>
