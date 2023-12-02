9f10b708067367dd1fc1e13c454ea387d321af4f
Sukrit <sukritgoyal36@gmail.com>
2023-12-02 22:26:29 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from "./Components/NavBar";
 import './App.css';
-import { BrowserRouter as Router , Routes , Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router , Routes , Route} from "react-router-dom";
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
