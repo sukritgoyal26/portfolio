931f3431d0f05aa4e3f182bae8ac6358702b7a73
Sukrit <sukritgoyal36@gmail.com>
2023-12-02 14:52:16 +0530

    fjj

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from "./Components/NavBar";
 import './App.css';
-import { BrowserRouter as Router , Routes , Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
@@ -18,8 +18,6 @@ function App() {
           <Route path="/projects" element = {<Projects />} />
           <Route path="/experience" element = {<Experience />} />
         </Routes>
-      </BrowserRouter>
-        
         <Footer />
       </Router>
     </div>
